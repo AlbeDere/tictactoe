ea4781ae5929a4f67e7eb17642120b6ce17b1416
Albert D <a.derevski@gmail.com>
2024-01-30 20:35:26 +0200

    main.py cleanup

--- tictactoe.py
+++ tictactoe.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/play', methods=['POST'])
-def play():
-    return jsonify({'status': 'success', 'message': 'Move successful'})
-
 if __name__ == '__main__':
     app.run(debug=True)
